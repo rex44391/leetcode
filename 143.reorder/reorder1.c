f151a4da6db6aa851b5df7ec42f93d72f1a8d0ca
TC Lien <rex44391@gmail.com>
2018-07-27 21:14:26 +0800

    push reorder 1

---
+++ 143.reorder/reorder1.c
@@ -0,0 +1,39 @@
+/* In this version, I use an array to record all pointer
+* and therefore, I don't need to get the tail by searching all the list everytime
+* although the space complexity goes up to O(n)
+* the improvement of time complexity is significant(from O(n!) to O(n)),
+* It beats 100% of other submissions(the last version is the slowest!)
+* improving the execution time from 686 ms to 8ms!!! 
+*/
+
+void reorderList(struct ListNode* head) {
+    int l_len = 0;
+    struct ListNode *ptr = head;
+    while(ptr) {
+        l_len++;
+        ptr = ptr->next;
+    }
+    struct ListNode **table = malloc(sizeof(struct ListNode) * l_len);
+    ptr = head;
+    for(int i = 0; i < l_len; i++) {
+        table[i] = ptr;
+        ptr = ptr->next;
+    }
+    
+    int left = 0;
+    int right = l_len - 1;
+    struct ListNode **pptr = &head;
+    while(left <= right) {
+        if(left == right) {
+            *pptr = table[left];
+            (*pptr)->next = NULL;
+            break;
+        }
+        *pptr = table[left];
+        (*pptr)->next = table[right];
+        (*pptr)->next->next = NULL;
+        pptr = &((*pptr)->next->next);
+        left++;
+        right--;
+    }
+}
